adeaaa40083accff428f9820b536af32684216d5
hanyi1994 <570654842@qq.com>
2021-03-22 20:09:14 +0800

    Mesh Deformer v0.1

---
+++ myMeshDeformation/Assets/MeshDeformer.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(MeshFilter))]
+public class MeshDeformer : MonoBehaviour
+{
+    Mesh deformingMesh;
+    Vector3[] originalVertices, displacedVertices;
+    Vector3[] vertexVelocities;
+
+    private void Start()
+    {
+        // 获得初始的 mesh 顶点位置，并储存到 deform 数组中
+        deformingMesh = GetComponent<MeshFilter>().mesh;
+        originalVertices = deformingMesh.vertices;
+        displacedVertices = new Vector3[originalVertices.Length];
+        vertexVelocities = new Vector3[originalVertices.Length]; ;
+        for (int i = 0; i < originalVertices.Length; i++)
+        {
+            displacedVertices[i] = originalVertices[i];
+        }
+    }
+    public void AddDeformingForce(Vector3 point, float force)
+    {
+        Debug.DrawLine(Camera.main.transform.position, point);
+        for (int i = 0; i < displacedVertices.Length; i++)
+        {
+            AddForceToVertex(i, point, force);
+        }
+    }
+
+    void AddForceToVertex(int i, Vector3 point, float force)
+    {
+        Vector3 pointToVertex = displacedVertices[i] - point;
+        float attenuatedForce = force / (1f + pointToVertex.sqrMagnitude);
+        float velocity = attenuatedForce * Time.deltaTime;  // dv = F dt
+        vertexVelocities[i] += pointToVertex.normalized * velocity;
+    }
+    void Update()
+    {
+        for (int i = 0; i < displacedVertices.Length; i++)
+        {
+            UpdateVertex(i);
+        }
+        deformingMesh.vertices = displacedVertices;
+        deformingMesh.RecalculateNormals(); // 重新计算法线
+    }
+    void UpdateVertex(int i)
+    {
+        Vector3 velocity = vertexVelocities[i];
+        displacedVertices[i] += velocity * Time.deltaTime;  // 逐渐变形
+    }
+}
---
+++ myMeshDeformation/Assets/MeshDeformerInput.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MeshDeformerInput : MonoBehaviour
+{
+    public float force = 10f;
+    public float forceOffset = 0.1f;
+
+    void Update()
+    {
+        if (Input.GetMouseButton(0))    // 触发 deformation 条件
+        {
+            HandleInput();
+        }
+    }
+
+    void HandleInput()
+    {
+        Ray inputRay = Camera.main.ScreenPointToRay(Input.mousePosition);   // Ray 的数据结构是两个 Vec3，分别是 origin, direction
+        //  这里是从 camera 到 a screen point.
+
+        RaycastHit hit;
+        // hit 是储存Raycast相关信息的数据结构
+        if (Physics.Raycast(inputRay, out hit)) // out 可以让函数传递出多个返回值
+        {           
+            MeshDeformer deformer = hit.collider.GetComponent<MeshDeformer>();  // 挂载在 与Ray碰撞的 mesh 上的 deformer
+            if (deformer)
+            {
+                Vector3 point = hit.point;
+                point += hit.normal * forceOffset;  // 在一个很接近表面的位置
+                deformer.AddDeformingForce(point, force);
+            }
+        }
+    }
+}
